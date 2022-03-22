5cd2136599e4547b9152aeb39a3176fa1910b16d
unknown <rafaelhazevedo61@gmail.com>
2022-03-21 23:50:23 -0300

    commit aula 1 - parcial - ordenacao

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,7 +1,6 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
@@ -11,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,9 +43,9 @@ public class TopicosController {
 
 	@GetMapping
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina,
-			@RequestParam int qtd) {
+			@RequestParam int qtd, @RequestParam String ordenacao) {
 		
-		Pageable paginacao = PageRequest.of(pagina, qtd);
+		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.DESC, ordenacao);
 		
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
