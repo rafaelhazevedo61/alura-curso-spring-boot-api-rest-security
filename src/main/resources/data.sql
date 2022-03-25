61625565707f03f763bccc549ccbab2cb9fce9d0
unknown <rafaelhazevedo61@gmail.com>
2022-03-24 23:29:36 -0300

    aula 3 - completa - autenticando o usuario

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) 
+VALUES('Aluno', 'aluno@email.com', '$2a$10$C3e6TB6y/e7k7C.s0RqZEe2CEqM6CSsfrAC.2Qt/YbLtmDSpKgHQC');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
