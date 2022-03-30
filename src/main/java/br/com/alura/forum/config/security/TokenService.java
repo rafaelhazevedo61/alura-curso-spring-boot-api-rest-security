b894098df7f38e0d00c74e1fdb44f24ffcaed237
rafael <rafaelhazevedo61@gmail.com>
2022-03-30 00:34:50 -0300

    aula 5 - completa

--- src/main/java/br/com/alura/forum/config/security/TokenService.java
+++ src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.forum.modelo.Usuario;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -24,8 +25,12 @@ public class TokenService {
 		Date hoje = new Date();
 		Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
 
-		return Jwts.builder().setIssuer("API do Fórum da Alura").setSubject(logado.getId().toString()).setIssuedAt(hoje)
-				.setExpiration(dataExpiracao).signWith(SignatureAlgorithm.HS256, secret).compact();
+		return Jwts.builder().setIssuer("API do Fórum da Alura")
+				.setSubject(logado.getId().toString())
+				.setIssuedAt(hoje)
+				.setExpiration(dataExpiracao)
+				.signWith(SignatureAlgorithm.HS256, secret)
+				.compact();
 	}
 
 	public boolean isTokenValido(String token) {
@@ -36,6 +41,14 @@ public class TokenService {
 		} catch (Exception e) {
 			return false;
 		}
+	}
+
+	public Long getIdUsuario(String token) {
+		
+		Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
+		return Long.parseLong(claims.getSubject());
+
+		
 	}
 
 }
